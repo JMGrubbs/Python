ac528b962b3fd78482104b7f9f2d2cc5e36f01a4
jgrubbs4 <jgrubbs4@dxc.com>
2020-03-09 13:30:09 -0500

    dayStart

---
+++ helloWorld/escapes.py
@@ -0,0 +1,9 @@
+splitString = 'this string has \nescapes \nin \nit'
+print(splitString)
+
+
+tabbedString = 'this string has \ttabs........ \tin \tit'
+print(tabbedString)
+
+
+threeQuotes = """"You dont need to escape anything if you have triple quotes"""
---
+++ helloWorld/printingStrings.py
@@ -0,0 +1,11 @@
+print('This is a good day to learn python')
+print('python is fun')
+print('Python strings are easy')
+print('Python strings use quotes')
+print('You can add' +  'strings to eachother')
+
+name = input('Please enter your name here')
+greeting = 'oh hellow, '
+
+
+print(name +' '+ greeting) 
